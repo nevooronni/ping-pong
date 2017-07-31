2ea4c4a37b73abe487cbfab7b4f990735baf7f82
Neville Oronni <nevooronni@gmail.com>
2017-07-31 21:58:06 +0300

    new variable

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 //custom js file
 $(document).ready(function() {
+	//back-end logic
+	var start = 1;
+	var end = parseInt($("input[name='number']").val());
 	
 //front-end/user-interface
 	$("#position").hide();
 	$("#output").hide();
 	$("#button").click(function() {
-		//$("#disapear").hide();
-		$("#position").show();
-		$("#disapear").hide();
+
+		$("div#position").show();
+		$("div#disapear").hide();
 	});
 		//button two that returns the array
-	$("#button2").click(function() {
-  	$("#append").text();
-  	$("#output").show();
+	$("button#button2").click(function(event) {
+  	$("p#append").text(end);
+  	$("div#output").show();
+  	event.preventDefault();
   });
 
+	
+
+	//var hard = function range(start, end) {		
+		////var array = new Array();
+
+		//for(var i = start; i < end; i++) {
+			//array.push(i);
+		//}
+	//}
 });
