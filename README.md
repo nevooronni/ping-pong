90e5e77554b9483b53a79b930197b4ad67c16d7e
Neville Oronni <nevooronni@gmail.com>
2017-08-01 16:10:33 +0300

    final changes

---
+++ README.md
@@ -0,0 +1,39 @@
+# PING PONG application
+
+## PROJECT 3
+
+## AUTHOR
+Neville Oronni
+
+## DESCRIPTION
+web application that takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:
+
+Numbers divisible by 3 are replaced with "ping"
+Numbers divisible by 5 are replaced with "pong"
+Numbers divisible by 15 are replaced with "pingpong"
+A user should be able to enter a new number and see new results over and over again.
+
+## SETUP/INTSTALLATION
+Requires js enbled browser
+
+## TECHNOLOGIES USED  
+* HTML
+* CSS
+* BOOTSTRAP
+* JS/JQUERY
+
+## SPECIFICATION
+
+Program will | e.g Input | e.g Output |
+----- |----- | -----
+count up to the number entered by user | number: 5 | show: [1,2,3,4,5,]
+number 3 will instead show "ping" |number: 3 | show: [1,2,ping]
+number 5 will instead show "pong"| number: 5 | show: [1,2,ping,4,pong]  
+number15 will instead show "pingpong" | number: 15 | show: [1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong]
+Replace the number 15 with the word "pingpong" | number: 15 |  show:1,2,ping,4,pong,ping,7,8,ping,pong,11,ping,13,14,pingpong]
+
+## CONTACT
+email:[nevooronni@gmail.com](nevoronni@gmail.com)
+
+## LICENSE
+*MIT LICENCE*
--- js/script.js
+++ js/script.js
@@ -1,13 +1,34 @@
-//custom js file
-$(document).ready(function() {
-	//back-end logic
-	
-//front-end/user-interface
-	$("button#button2").click(function(event) {
-		var userInput = parseInt($("input#input").val());//takes user iput
-		//var resultOfArray = pingpongLoop(resultOfArray);//uses user input as argument to function and return the sequence
-  	$("p#append").text(userInput);
-  	event.preventDefault();
-  });
+//custom script
 
+//back-end code 
+var pingpongLoop = function(userInput) {///variable that starts the loop
+	var resultOfArray = [];
+	for(var i = 1; i <= userInput; i += 1) {//loop
+		if(((i % 3) === 0) && ((i % 5) === 0)) {//1condition
+			resultOfArray.push("pingpong");
+		} else if ((i % 3) === 0) {//second condition
+			resultOfArray.push("ping");
+		} else if ((i % 5) === 0) {//third condition
+			resultOfArray.push("pong");
+		} else {
+			resultOfArray.push(i);//outcome
+		}
+	}
+	return resultOfArray;
+}
+
+
+//Front-end jquery code
+$(document).ready(function() {
+	//loop that generates the number sequence 
+$("#button2").click(function(event) {
+		event.preventDefault();
+		var userInput = parseInt($("#enter").val());//takes user iput
+		var resultOfArray = pingpongLoop(userInput);//uses user input as argument to function and return the sequence
+  	$("#append").text(resultOfArray);
+  });
+	$("#reset").click(function(event) {
+    location.reload();
+  });
 });
+
