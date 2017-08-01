7966b79e2a115e13f8b24e892203a2fc8de67ead
Neville Oronni <nevooronni@gmail.com>
2017-08-01 04:37:19 +0300

    added user interface

--- js/script.js
+++ js/script.js
@@ -1,31 +1,13 @@
 //custom js file
 $(document).ready(function() {
 	//back-end logic
-	var start = 1;
-	var end = parseInt($("input[name='number']").val());
 	
 //front-end/user-interface
-	$("#position").hide();
-	$("#output").hide();
-	$("#button").click(function() {
-
-		$("div#position").show();
-		$("div#disapear").hide();
-	});
-		//button two that returns the array
 	$("button#button2").click(function(event) {
-  	$("p#append").text(end);
-  	$("div#output").show();
+		var userInput = parseInt($("input#input").val());//takes user iput
+		//var resultOfArray = pingpongLoop(resultOfArray);//uses user input as argument to function and return the sequence
+  	$("p#append").text(userInput);
   	event.preventDefault();
   });
 
-	
-
-	//var hard = function range(start, end) {		
-		////var array = new Array();
-
-		//for(var i = start; i < end; i++) {
-			//array.push(i);
-		//}
-	//}
 });
